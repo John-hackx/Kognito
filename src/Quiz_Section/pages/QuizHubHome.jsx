940b8ffc80e64bb7e936cd0588dcee957c731a65
John Hackx <myofficialsat@gmail.com>
2025-05-13 23:38:37 -0400

    removed alert and optimized view for samsung a15 series and others

--- src/Quiz_Section/pages/QuizHubHome.jsx
+++ src/Quiz_Section/pages/QuizHubHome.jsx
@@ -1,6 +1,6 @@
 import QuizHubMain from "../components/QuizHubMain";
 import Footer from "../../Main_App/components/Footer";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { WindowSizeContext } from "../../Main_App/components/WindowSizeContext";
 import MobileFooter from "../../Main_App/components/MobileFooter";
 
@@ -8,9 +8,12 @@ function QuizHubDashboard() {
   const { windowWidth } = useContext(WindowSizeContext);
   const mobileView = windowWidth <= 500;
 
-  useEffect(function () {
-    alert(`${windowWidth} X ${window.innerHeight}`);
-  }, []);
+  // useEffect(
+  //   function () {
+  //     alert(`${windowWidth} X ${window.innerHeight}`);
+  //   },
+  //   [windowWidth]
+  // );
 
   return (
     <>

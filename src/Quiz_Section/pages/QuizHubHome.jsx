b7b56723cdedd487c7789287b049c359111aa187
John Hackx <myofficialsat@gmail.com>
2025-05-13 23:22:59 -0400

    added alert function on quiz hub home to check somting

--- src/Quiz_Section/pages/QuizHubHome.jsx
+++ src/Quiz_Section/pages/QuizHubHome.jsx
@@ -1,12 +1,16 @@
 import QuizHubMain from "../components/QuizHubMain";
 import Footer from "../../Main_App/components/Footer";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { WindowSizeContext } from "../../Main_App/components/WindowSizeContext";
 import MobileFooter from "../../Main_App/components/MobileFooter";
 
 function QuizHubDashboard() {
   const { windowWidth } = useContext(WindowSizeContext);
   const mobileView = windowWidth <= 500;
+
+  useEffect(function () {
+    alert(`${windowWidth} X ${window.innerHeight}`);
+  }, []);
 
   return (
     <>
